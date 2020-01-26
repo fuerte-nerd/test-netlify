ac5c30a6ea1e82f927a18e9599d84f43803ca7bf
fuerte-nerd <fuertenerd@gmail.com>
2020-01-26 17:32:09 +0000

    testing

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,4 +1,18 @@
 const path = require("path")
+const { createFilePath } = require("gatsby-source-filesystem")
+
+exports.onCreateNode = ({ node, boundActionCreators, getNode }) => {
+  const { createNodeField } = boundActionCreators
+
+  if (node.internal.type === `MarkdownRemark`) {
+    const value = createFilePath({ node, getNode })
+    createNodeField({
+      name: `slug`,
+      node,
+      value,
+    })
+  }
+}
 
 exports.createPages = async ({ actions, graphql, reporter }) => {
   const { createPage } = actions
@@ -11,6 +25,9 @@ exports.createPages = async ({ actions, graphql, reporter }) => {
       ) {
         edges {
           node {
+            fields {
+              slug
+            }
             frontmatter {
               path
             }
@@ -24,11 +41,11 @@ exports.createPages = async ({ actions, graphql, reporter }) => {
     reporter.panicOnBuild("Error while running GraphQL query.")
     return
   }
-  result.data.allMarkdownRemark.edges.forEach(({ node })=>{
-      createPage({
-          path: node.frontmatter.path,
-          component: blogPostTemplate,
-          context: {}
-      })
+  result.data.allMarkdownRemark.edges.forEach(({ node }) => {
+    createPage({
+      path: node.fields.slug,
+      component: blogPostTemplate,
+      context: {},
+    })
   })
 }
